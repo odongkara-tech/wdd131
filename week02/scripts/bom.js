f3145aae818842809a08504302e51641974cb525
odongkara-tech <rodongkara@byupathway.edu>
2026-09-15 16:32:11 +0300

    Add Book of Mormon chapter app

---
+++ week02/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+	const chapter = input.value.trim();
+	if (!chapter) {
+		return;
+	}
+
+	const listItem = document.createElement("li");
+	const deleteButton = document.createElement("button");
+	deleteButton.textContent = "❌";
+	deleteButton.classList.add("delete");
+	deleteButton.type = "button";
+	deleteButton.addEventListener("click", () => {
+		listItem.remove();
+		input.focus();
+	});
+	listItem.textContent = chapter;
+	listItem.appendChild(deleteButton);
+	list.appendChild(listItem);
+	input.value = "";
+	input.focus();
+});
